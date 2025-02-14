40c29adc742ca0c76a91c1ca3ef1d03ade5d8242
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-14 19:58:38 +0800

    Add files via upload

---
+++ javacode/Main17.java
@@ -0,0 +1,13 @@
+public class Main17 {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 100;
+		int z = 200;
+		System.out.println(x == y); // true
+		System.out.println(x != z); // true
+		System.out.println(x >= y); // true
+		System.out.println(x > y);  // false
+		System.out.println(x <= y); // true
+		System.out.println(x < y);  // false
+	}
+}
