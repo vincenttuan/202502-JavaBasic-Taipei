ffdaf30a5f4d006e607767493d988972a33ea1a7
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-14 21:11:49 +0800

    Add files via upload

--- javacode/Main20.java
+++ javacode/Main20.java
@@ -6,7 +6,31 @@
 */
 public class Main20 {
 	public static void main(String[] args) {
-		
-		
+		// 假設的環境條件
+		int lux = 80; // 光線強度
+		int tmp = 30; // 房間溫度
+		boolean hasPerson = true; // 房間是否有人?
+
+		// 控制燈光
+		boolean light = (lux < 100) && (hasPerson == true);
+		if(light == true) {
+			System.out.println("燈光已開啟");
+		} else {
+			System.out.println("燈光已關閉");
+		}
+
+		// 控制空調
+		boolean ac = (tmp > 28) && (hasPerson == true);
+		if(ac == true) {
+			System.out.println("空調已開啟");
+		} else {
+			System.out.println("空調已關閉");
+		}
+
+		// 如果房間都沒人關閉燈光與空調
+		if(!(hasPerson == true)) {
+			System.out.println("房間都沒人關閉燈光與空調");
+		}
+
 	}
 }
