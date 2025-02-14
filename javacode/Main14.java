58c366ab9c53b525bf9abe3bf234617c04f99521
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-14 19:11:14 +0800

    Add files via upload

---
+++ javacode/Main14.java
@@ -0,0 +1,11 @@
+public class Main14 {
+
+	public static void main(String[] args) {
+		int score = 75;
+		boolean isPass = score >= 60;
+		System.out.printf("成績: %d%n", score);
+		System.out.printf("及格: %b%n", isPass);
+
+	}
+
+}
