6a07e9591eb0be87a3d4ef4a01e06020bd323cac
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-14 18:56:54 +0800

    Add files via upload

---
+++ javacode/Main13.java
@@ -0,0 +1,12 @@
+public class Main13 {
+
+	public static void main(String[] args) {
+		char a1 = 'A';
+		char a2 = 65; // ASCII(65) = 'A'
+		char a3 = '\u0041'; // unicode(0041) = 'A'
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+	}
+
+}
