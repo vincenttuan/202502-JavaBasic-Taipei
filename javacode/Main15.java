1ee3a4f8eeb76f24b5bb8fbfda17fae970c87ef2
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-14 19:33:00 +0800

    Add files via upload

---
+++ javacode/Main15.java
@@ -0,0 +1,12 @@
+public class Main15 {
+	public static void main(String[] args) {
+		int age = 10;
+		//age = age + 1;
+		//age++;
+		//++age;
+		//age = age - 1;
+		//age--;
+		--age;
+		System.out.printf("age = %d%n", age);
+	}
+}
