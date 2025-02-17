f0e11a31a008e77c012ae889d4b2f942524f23ed
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-17 21:02:02 +0800

    Add files via upload

--- javacode/Main28.java
+++ javacode/Main28.java
@@ -7,7 +7,30 @@ public class Main28 {
 		// 請輸入運算符號(+-*/): +   <-- String op = sc.next();
 		// 請輸入第二個數字: 5       <-- int y = sc.nextInt(); 
 		// 得到答案: 15
-		
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("請輸入第一個數字:");
+		int x = sc.nextInt();
+		System.out.print("請輸入運算符號(+-*/):");
+		String op = sc.next();
+		System.out.print("請輸入第二個數字:");
+		int y = sc.nextInt();
+
+		System.out.print("得到答案:");
+		switch(op) {
+			case "+":
+				System.out.println(x+y);
+				break;
+			case "-":
+				System.out.println(x-y);
+				break;
+			case "*":
+				System.out.println(x*y);
+				break;
+			case "/":
+				System.out.println(x/y);
+				break;
+			default:
+				System.out.println("運算符號錯誤");
+		}
 	}
 }
