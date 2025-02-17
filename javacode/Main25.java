7d37240bd996774b3df8586f62618a6e15beb9e4
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-17 19:59:13 +0800

    Add files via upload

--- javacode/Main25.java
+++ javacode/Main25.java
@@ -26,5 +26,8 @@ public class Main25 {
 			System.out.println("正常");
 		}
 
+		// bmi 診斷狀態利用三元運算子嵌套寫出
+		String result = (bmi <= 18)?"過輕"?(bmi > 23)?"過重":"正常";
+		System.out.printf("診斷: %s%n", result);
 	}
 }
