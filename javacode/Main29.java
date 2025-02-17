04d405789f0f19e825181efae78d124c59cad25c
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-17 21:28:22 +0800

    Add files via upload

--- javacode/Main29.java
+++ javacode/Main29.java
@@ -48,7 +48,17 @@ public class Main29 {
 				break;			
 		}
 
-		
+		// swicth-case lambda 的寫法
+		String season = switch(month) {
+			case 12, 1, 2 -> "冬";
+			case 3, 4, 5 -> "春";
+			case 6, 7, 8 -> "夏";
+			case 9, 10, 11 -> "秋";
+			default -> "月份錯誤";
+		};
+
+		System.out.println(season);
+
 
 	}
 
