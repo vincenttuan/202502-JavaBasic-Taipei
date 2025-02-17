bce6007584ebd2cbd9c643a2609077b85ca9b13b
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-17 20:44:01 +0800

    Add files via upload

---
+++ javacode/Main28.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main28 {
+	public static void main(String[] args) {
+		// 系統使用情境:
+		// 請輸入第一個數字: 10      <-- int x = sc.nextInt();
+		// 請輸入運算符號(+-*/): +   <-- String op = sc.next();
+		// 請輸入第二個數字: 5       <-- int y = sc.nextInt(); 
+		// 得到答案: 15
+		
+		
+	}
+}
