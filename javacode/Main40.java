7d39018010b02c665a10c0d9005dec1d99802c90
VincentTuan <vincentjava@yahoo.com.tw>
2025-02-21 21:40:58 +0800

    Add files via upload

--- javacode/Main40.java
+++ javacode/Main40.java
@@ -1,17 +1,18 @@
 public class Main40 {
 	public static void main(String[] args) {
-		
+		int sum = 0;
 		for(int x=1;x<=9;x++) {
-
 			for(int y=1;y<=9;y++) {
 				//System.out.printf("%dx%d=%d ", x, y, (x*y));
 				//System.out.printf("%dx%d=%d\t", x, y, (x*y));
 				//System.out.printf("%2dx%-2d=%-3d ", x, y, (x*y));
 				System.out.printf("%02dx%02d=%03d ", x, y, (x*y));
+				sum += (x*y);
 			}
 			System.out.println();
-
 		}
+
+		System.out.printf("總和 = %,d%n", sum);
 
 	}
 }
